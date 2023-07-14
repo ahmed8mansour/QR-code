36c16f49eb26b9e837b0bec70f8e238bd1bf89a4
ahmed8mansour <a.m424134567@gmial.com>
2023-07-14 17:32:22 +0300

    first commit

---
+++ Qr code/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Qr_code
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QRCoder.QRCodeGenerator qg = new QRCoder.QRCodeGenerator();
+            var data = qg.CreateQrCode(textBox1.Text, QRCoder.QRCodeGenerator.ECCLevel.H);
+            var code = new QRCoder.QRCode(data);
+            pictureBox1.Image = code.GetGraphic(50);
+        }
+    }
+}
